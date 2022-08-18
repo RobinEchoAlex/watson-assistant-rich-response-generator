d5f60ce35790345e9ca68391ca5df2ae8df908a7
RobinEchoAlex <daxiafan7@gmail.com>
2022-08-18 23:16:54 +0100

    add basic dom manipulation feature

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
+//https://developer.chrome.com/docs/extensions/mv3/content_scripts/
+//this article needed to be improved to include necessary scripting permission for its example in "inject programmatically" section to work
+//also, a "host permission" in manifest is needed to be added
+
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    console.log("trigger")
-
+    console.log(tab)
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content-script.js']
+    });
   }
 })
+
+
+function isWatsonWebsite(url){
+  if (typeof url !== "string"){
+
+  }
+}
