4160fb0debbd88af974cb583b66daf82e455dd62
RobinEchoAlex <daxiafan7@gmail.com>
2022-08-19 01:09:35 +0100

    add basic button to the toolbar

--- content-script.js
+++ content-script.js
@@ -8,5 +8,38 @@ function main(){
     return
   }
   const toolBar = toolBars[0]
-  toolBar.style.backgroundColor = "#ff0000"
+  console.log(toolBar)
+
+  const button = buttonCreator("image")
+  toolBar.appendChild(button)
+}
+
+function buttonCreator(id){
+  const button = document.createElement("button")
+  button.id = "RichTextEditor--toolbar--toolbar-button--" + id
+  button.tabIndex = -1
+  button.textContent = "id"
+  addButtonClass(button)
+  button.onclick = function (){
+    alert("click")
+  }
+
+  return button
+}
+
+function addButtonClass(button){
+  const classList = ["ToolbarButton",
+    "bx--btn",
+    "bx--btn--sm",
+    "bx--btn--ghost",
+    "bx--btn--disabled",
+    "bx--tooltip--hidden",
+    "bx--btn--icon-only",
+    "bx--tooltip__trigger",
+    "bx--tooltip--a11y",
+    "bx--btn--icon-only--top",
+    "bx--tooltip--align-center"]
+  for(let i=0 ; i<classList.length;i++){
+    button.classList.add(classList[i])
+  }
 }
