ec860ba1ef177745015887f675cfce2bea2c5a42
RobinEchoAlex <daxiafan7@gmail.com>
2022-08-19 17:16:35 +0100

    add comment for dependencies script inclusion logic

--- background.js
+++ background.js
@@ -5,6 +5,10 @@
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     console.log(tab)
+    //If you just inject dependencies with static declarations (see
+    // https://developer.chrome.com/docs/extensions/mv3/content_scripts/)
+    //the content-script cannot find the static dependency
+    // so the dependencies are also dynamically injected here
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ['sweetalert2.all.min.js']
