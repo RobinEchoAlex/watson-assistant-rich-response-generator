b00b8e8e54b76cb7b30face35779c5c0f071b3bd
RobinEchoAlex <daxiafan7@gmail.com>
2022-08-19 01:10:33 +0100

    add js for popup form

--- content-script.js
+++ content-script.js
@@ -1,33 +1,69 @@
 main()
 
-function main(){
+function main() {
+  formFactory()
   const toolBars = document.getElementsByClassName("RichTextEditorToolbar")
-  if (toolBars==null){
+  if (toolBars == null) {
     return
-  }else if (toolBars.length>1){
+  } else if (toolBars.length > 1) {
     return
   }
   const toolBar = toolBars[0]
+  if (toolBar===undefined){
+    return
+  }
   console.log(toolBar)
 
   const button = buttonCreator("image")
   toolBar.appendChild(button)
+
 }
 
-function buttonCreator(id){
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function formFactory() {
+  if (document.getElementById("myForm")){
+    return
+  }
+
+  const popup = document.createElement("div")
+  popup.innerHTML = `<div class="form-popup" id="myForm" style="display: none">
+        <form action="/action_page.php" class="form-container">
+          <h1>Login</h1>
+      
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required>
+      
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required>
+      
+          <button type="submit" class="btn">Login</button>
+          <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+      </div>`
+  document.getElementsByClassName("RichTextEditorToolbar")[0].appendChild(popup)
+}
+
+function buttonCreator(id) {
   const button = document.createElement("button")
   button.id = "RichTextEditor--toolbar--toolbar-button--" + id
   button.tabIndex = -1
   button.textContent = "id"
   addButtonClass(button)
-  button.onclick = function (){
-    alert("click")
+  button.onclick = function () {
+    openForm()
   }
 
   return button
 }
 
-function addButtonClass(button){
+function addButtonClass(button) {
   const classList = ["ToolbarButton",
     "bx--btn",
     "bx--btn--sm",
@@ -39,7 +75,19 @@ function addButtonClass(button){
     "bx--tooltip--a11y",
     "bx--btn--icon-only--top",
     "bx--tooltip--align-center"]
-  for(let i=0 ; i<classList.length;i++){
+  for (let i = 0; i < classList.length; i++) {
     button.classList.add(classList[i])
   }
+}
+
+function addStyleSheet(){
+
+  let head = document.getElementsByTagName('HEAD')[0];
+  let cssLinkElem = document.createElement('link');
+
+  cssLinkElem.type = 'text/css';
+  cssLinkElem.rel = 'stylesheet';
+  cssLinkElem.href = 'popupImage.css';
+
+  head.appendChild(cssLinkElem);
 }
